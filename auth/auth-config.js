e4d3158658644ad15370c47af7b2fc22d983b716
PistoleroJack <tvb20072007@gmail.com>
2025-08-12 18:34:29 +0200

    Create auth-config.js

---
+++ auth/auth-config.js
@@ -0,0 +1,6 @@
+window.authConfig = {
+    domain: "kersmc.eu.auth0.com",
+    clientId: "GgvPQ4hrRY1i3nDAetOXQRFuHm5T5r7y",
+    audience: "", // Alleen nodig als je API gebruikt
+    redirectUri: window.location.origin + "/auth/callback.html"
+};
